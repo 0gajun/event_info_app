4c16cbbd63899f9affd8401eb3d0db52d99d4d21
OGA-Mac <oga.ivc.s27@gmail.com>
2014-07-24 21:42:51 +0900

    modified static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,17 @@ describe "StaticPages" do
 	describe "Home page" do 
 		it "should have the content '矢上祭出展管理システム'" do 
 			visit root_path
-			expect(page).to have_content('矢上祭出展管理システム')
+			expect(page).to have_title(full_title(''))
 		end
 	end
 
 	describe "About page" do 
+		before { visit about_path }
 		it "should have the content '当システムについて'" do 
-			visit about_path
 			expect(page).to have_content('当システムについて')
+		end
+		it "should have the title '当システムについて'" do 
+			expect(page).to have_title(full_title('当システムについて'))
 		end
 	end
 
