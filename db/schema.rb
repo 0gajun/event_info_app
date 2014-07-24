89d6bdf1f59538a62c2f916c09b98bfd5f84ae97
OGA-Mac <oga.ivc.s27@gmail.com>
2014-07-25 00:52:15 +0900

    パスワード追加開始

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724124959) do
+ActiveRecord::Schema.define(version: 20140724154924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20140724124959) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
