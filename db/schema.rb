9be6c2c7599effa854e384bbdcec24b353e61016
OGA-Mac <oga.ivc.s27@gmail.com>
2014-08-01 00:42:24 +0900

    UserモデルにManagerId追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731095945) do
+ActiveRecord::Schema.define(version: 20140731153902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140731095945) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.integer  "managerId"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
